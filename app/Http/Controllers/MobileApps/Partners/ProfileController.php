6c1bdb939ad5330aa47da490440fb835fa5b628c
pks11iitr <pks11iitr@gmail.com>
2021-10-09 11:47:18 +0530

    bank kyc apis

--- app/Http/Controllers/MobileApps/Partners/ProfileController.php
+++ app/Http/Controllers/MobileApps/Partners/ProfileController.php
@@ -3,13 +3,52 @@
 namespace App\Http\Controllers\MobileApps\Partners;
 
 use App\Http\Controllers\Controller;
+use App\Http\Controllers\Traits\FileTransfer;
 use Illuminate\Http\Request;
 
 class ProfileController extends Controller
 {
+    use FileTransfer;
+
     public function updateKYC(Request $request){
 
         $user = $request->user;
+
+        $request->validate([
+            'pan_no'=>'required',
+            'aadhaar_no'=>'required'
+        ]);
+
+        if(empty($user->pan_url) || empty($user->adhaar_url)){
+
+            if(empty($request->pan_image) || empty($request->aadhaar_image))
+                return [
+                    'status'=>'failed',
+                    'action'=>'',
+                    'display_message'=>'Please upload pan & aadhaar images',
+                    'data'=>[],
+                ];
+        }
+
+
+        $pan_url = $this->getImagePath($request->pan_image, 'user/'.$user->id.'pan');
+        $aadhaar_url = $this->getImagePath($request->pan_image, 'user/'.$user->id.'aadhaar');
+
+        $user->update([
+            'pan_no'=>$request->pan_no,
+            'aadhaar_no'=>$request->aadhaar_no,
+            'aadhaar_url'=>$aadhaar_url,
+            'pan_url'=>$pan_url
+        ]);
+
+        return [
+            'status'=>'success',
+            'action'=>'',
+            'display_message'=>'KYC Details Updated Successfully',
+            'data'=>[],
+        ];
+
+
 
     }
 
