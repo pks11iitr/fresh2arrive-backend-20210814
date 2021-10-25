3475219ad09942422ef76f15e7f2259a39eb2783
pankaj sengar <pks11iitr@gmail.com>
2021-10-25 11:33:30 +0530

    siderbar info fixed

--- resources/views/service-areas.blade.php
+++ resources/views/service-areas.blade.php
@@ -32,18 +32,19 @@
         .service_area{box-shadow:0px 4px 4px rgb(0 0 0 / 6%);padding:15px;border:1px solid #ccc;border-radius: 6px;}
         .flex_flow{display: flex;}
         .align_center{align-items: center;}
-        .city_list li{width:31%;margin: 0px 0 20px;}
-        .city_list{display: flex;list-style-type: none;margin:15px 0 0;padding:0;flex-flow: wrap;justify-content:space-between;}
+        .city_list li{width:31%;margin: 0px 6px 20px;}
+        .city_list{display: flex;list-style-type: none;margin:15px 0 0;padding:0;flex-flow: wrap;justify-content:flex-start;}
         .city_list li a{display: inline-block;border:1px solid #F27935;border-radius: 3px;color: #333;
             font-family: 'Nunito', sans-serif;text-decoration: none;padding: 6px 0px;width: 100%;
             box-sizing: border-box;text-align: center;background: #FFECE2;color: #F27935;    font-size: 12px;}
         .f_20{font-size: 20px;}
         .servicing_area{border-top: 1px solid rgb(204 204 204 / 50%);padding: 20px 0 0;}
-        .servicble_area{margin:0;padding:0;list-style-type:none;font-family: 'Nunito';display: flex;justify-content: space-between;flex-flow: wrap;}
+        .servicble_area{margin:0;padding:0;list-style-type:none;font-family: 'Nunito';display: flex;justify-content: flex-start;flex-flow: wrap;}
         .servicble_area li{border: 1px solid #ccc;
             border-radius: 3px;
             padding: 8px 13px;
-            font-size: 12px;width: 25%;margin:15px 0 0}
+            font-size: 12px;width: 34%;
+            margin: 15px 10px 0;}
         .active_bld{font-weight: bold;}
         .servicing_area{display: none;}
 
@@ -51,7 +52,7 @@
 
             .main_wrapper{width: 98%;}
             .service_area{box-sizing: border-box;}
-            .city_list li{width: 22%;}
+            .city_list li{width:30%;}
 
         }
     </style>
