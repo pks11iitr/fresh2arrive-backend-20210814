b7e039ef1baca3fb2f3f9e1f7775f2ec29029659
pankaj sengar <pks11iitr@gmail.com>
2021-11-13 23:30:55 +0530

    location search changed

--- resources/views/admin/customers/view.blade.php
+++ resources/views/admin/customers/view.blade.php
@@ -73,13 +73,13 @@
                                         <th>Balance</th>
                                         <th>Status</th>
                                         <th>Action</th>
-                                        <th>Wallet</th>
+{{--                                        <th>Wallet</th>--}}
                                     </tr>
 
 
-                        <?php 
-                                      
-                                        
+                        <?php
+
+
                                         ?>
 
                                     </thead>
@@ -108,7 +108,7 @@
 
             <td>{{$status}}</td>
             <td><a href="{{route('customers.edit',['id'=>$customers->id])}}">Edit</a></td>
-            <td><a href="javascript:void(0)" class='btn btn-primary' onclick="openWalletPanel('{{$order->id??''}}', '{{route('user.wallet.balance', ['id'=>$customers->id])}}', {{$customers->id}})">Add/Revoke Balance</a></button></td>
+{{--            <td><a href="javascript:void(0)" class='btn btn-primary' onclick="openWalletPanel('{{$order->id??''}}', '{{route('user.wallet.balance', ['id'=>$customers->id])}}', {{$customers->id}})">Add/Revoke Balance</a></button></td>--}}
                                             </tr>
                                         @endforeach
                                     </tbody>
