3dd29800409f7a141a8b76d1f38f2f3a3ebfc2c1
pankaj sengar <pks11iitr@gmail.com>
2021-11-28 18:26:15 +0530

    Add money
    Signed-off-by: pankaj sengar <pks11iitr@gmail.com>

--- app/Http/Controllers/Admin/InvoiceController.php
+++ app/Http/Controllers/Admin/InvoiceController.php
@@ -15,7 +15,7 @@ class InvoiceController extends Controller
         $order_data=OrderDetail:: join('orders','orders.id','=','order_details.order_id')
             ->join('partners', 'partners.id', '=', 'orders.delivery_partner')
             ->join('customers', 'customers.id', '=', 'orders.user_id')
-            ->select('orders.refid as refrerid','orders.id as oid','orders.echo_charges','orders.created_at','orders.delivery_date','orders.bags_no','orders.crate_no','partners.name as pname','partners.address','partners.mobile as pmobile','partners.house_no as phouse_no','partners.city as pcity','partners.pincode as ppincode','partners.state as pstate',
+            ->select('orders.refid as refrerid','orders.id as oid','orders.echo_charges','orders.created_at','orders.delivery_date', 'orders.delivery_time','orders.bags_no','orders.crate_no','partners.name as pname','partners.address','partners.mobile as pmobile','partners.house_no as phouse_no','partners.city as pcity','partners.pincode as ppincode','partners.state as pstate',
                 'customers.name','customers.mobile','customers.city','customers.house_no','customers.building','customers.street','customers.area','customers.city','customers.state','customers.pincode')
             ->where('order_id',$orderid)
             ->first();
