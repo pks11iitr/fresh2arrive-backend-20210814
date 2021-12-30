ce1ddc662a3cc3f62c4d0f2f78186700769e1b0a
pankaj sengar <pks11iitr@gmail.com>
2021-12-30 17:21:12 +0530

    minor fixes

--- resources/views/admin/orders/sales-summary.blade.php
+++ resources/views/admin/orders/sales-summary.blade.php
@@ -7,7 +7,7 @@
             <div class="container-fluid">
                 <div class="row mb-2">
                     <div class="col-sm-6">
-                        <h1>Product Requirements</h1>
+                        <h1>Sales Report</h1>
                     </div>
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
