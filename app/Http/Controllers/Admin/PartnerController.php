a8fb16c099505ba62a45913b6d34052db538b4be
avaskm02 <avaskm02@example.com>
2022-07-24 13:29:22 +0530

    update

--- app/Http/Controllers/Admin/PartnerController.php
+++ app/Http/Controllers/Admin/PartnerController.php
@@ -28,7 +28,8 @@ class PartnerController extends Controller
                 ->paginate(10);
         }else{
             $partners = Partner::
-                orderBy('id', 'desc')
+                 orderBy('status', 'ASC')
+                //->orderBy('status','ASC')
                 ->paginate(10);
         }
         return view('admin.partners.view',compact('partners','areas'));
